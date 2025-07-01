fa2701b989091a3cd17cc62c625c58c66b1847ef
Riakol <inspi47@gmail.com>
2025-07-01 17:56:22 +0500

    feat(ui): implement navigation drawer and toolbar
    Add DrawerLayout with NavigationView for app navigation
    
    Integrate Toolbar as the main ActionBar
    
    Create navigation header layout (nav_header_main)
    
    Add necessary string and color resources
    
    Remove unused night theme to simplify theming

--- app/src/main/java/com/riakol/bulletinboard/MainActivity.kt
+++ app/src/main/java/com/riakol/bulletinboard/MainActivity.kt
@@ -1,20 +1,55 @@
 package com.riakol.bulletinboard
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import com.riakol.bulletinboard.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("View binding is not initialized")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init() {
+
+        with(binding) {
+            val toolBar = mainContent.toolbar
+            val toggle = ActionBarDrawerToggle(
+                this@MainActivity,
+                drawerLayout,
+                toolBar,
+                R.string.open,
+                R.string.close)
+            drawerLayout.addDrawerListener(toggle)
+            toggle.syncState()
+            navView.setNavigationItemSelectedListener(this@MainActivity)
         }
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+         when(item.itemId) {
+            R.id.id_ads -> Toast.makeText(this, "Ads clicked", Toast.LENGTH_SHORT).show()
+            R.id.id_car -> "Car clicked"
+            R.id.id_pc -> "PC clicked"
+            R.id.id_smartphone -> "Smartphone clicked"
+
+        }
+
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
